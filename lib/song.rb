6636ff5c2bdbb63a05b77835ddf96b1ae4d5162f
Kelly Sheddy <jungaka07@gmail.com>
2019-01-08 14:15:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 class Song
+  extend Memorable::ClassMethods
   attr_accessor :name
   attr_reader :artist
 
@@ -34,5 +35,5 @@ class Song
     name.downcase.gsub(' ', '-')
   end
   
-  extend Memorable::ClassMethods
+  
 end
